4b71e5dbf0c3491e119072eda59b479d4d13f37f
Koutilya Ganapathiraju <koutilyagraju@gmail.com>
2025-06-18 15:25:34 -0500

    Initial commit - maintenance log project only

---
+++ maintenance_log.py
@@ -0,0 +1,54 @@
+import csv
+import os
+from datetime import datetime
+
+notebook = []
+action = ''
+total_rows = 0
+
+filename = "maintenance_log.csv"
+fieldnames = ["Name of Machine", "Date of Maintenance", "Name of Technician", "Name of Maintenance", "Hours Since Last Service"]
+does_file_exist = os.path.exists(filename)
+
+try:
+    with open(filename, mode = 'a', newline = '') as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames, delimiter="\t")
+
+        if not does_file_exist:
+            writer.writeheader()
+        
+        while True:
+            action = input("Would you like to add a maintenance entry? ").casefold()
+
+            if action != "yes":
+                break
+            
+            log = {
+                "Name of Machine" : input("What machine? ").strip(),
+                "Date of Maintenance" : input("When was the date of maintenance? ").strip(),
+                "Name of Technician" : input("What is the name of the technician who worked on the machine? ").strip(),
+                "Name of Maintenance" : input("What was the maintenance performed? ").strip(),
+                "Hours Since Last Service" : input("How many hours has it been since the last service? ").strip()
+            }
+            log["Entry Time"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+            writer.writerow(log)
+            notebook.append(log)
+
+            print("New Entry Added.")
+            print("-" * 40)
+            for key, value in log.items():
+                print(f"{key}: {value}")
+            print("-" * 40)
+
+            with open(filename, mode='r', newline='') as f:
+                total_rows = sum(1 for _ in f) - 1
+
+except Exception as e:
+    print("An Error Occured.")
+    print(f"Error Details: {e}")
+
+
+print(f"\nTotal entries recorded: {len(notebook)}")
+print(f"Total entries in file: {total_rows}")
+print("Thanks and Goodbye!\n")
